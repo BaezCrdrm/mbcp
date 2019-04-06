4bdc80ba7bdc41b93473841dd755e40490a26307
BaezCrdrm <dev.baez_crdrm@outlook.com>
2019-04-05 20:53:16 -0600

    Inicialización de proceso

--- src/Model/Proceso.java
+++ src/Model/Proceso.java
@@ -8,26 +8,91 @@ import java.util.List;
  */
 public class Proceso
 {
-    String address, port, id;
+    String address, puerto, id;
     List<Mensaje> buffer, recibidos;
-    List<Proceso> procesos;
     List<Tupla> ci;
     int[] vt;
 
-
-    public Proceso(String ip, String port, String id)
+    /**
+     * Crea la definición de un proceso
+     * @param ip Dirección IP del equipo que ejecutará el proceso
+     * @param puerto Puerto del equipo que ejecutará el proceso
+     * @param id Identificador del proceso
+     */
+    public Proceso(String ip, String puerto, String id)
     {
         this.address = ip;
-        this.port = port;
+        this.puerto = puerto;
         this.id = id;
     }
 
-    public Proceso(List<Proceso> procesos, String ip)
+    /**
+     * Crea un nuevo proceso que será utilizado en el
+     * algoritmo
+     * @param proceso Proceso de definición del cual se obtendrán
+     * los datos del proceso
+     * @param n Número de procesos. Creará el vector de tamaño 'n'
+     */
+    public Proceso(Proceso proceso, int n)
     {
-        this.buffer = new ArrayList<Mensaje>();
-        this.procesos = procesos;
-        this.vt = new int[procesos.size()];
-        this.address = ip;
-        this.ci = new ArrayList<Tupla>();
+        try
+        {
+            this.id = proceso.getId();
+            this.address = proceso.getAddress();
+            this.puerto = proceso.getPuerto();
+
+            this.buffer = new ArrayList<Mensaje>();
+            this.vt = new int[n];
+            this.ci = new ArrayList<Tupla>();
+        } catch(NullPointerException npe)
+        {
+            System.out.println("Ocurró un error al crear el proceso");
+            System.out.println("Agrega los valores correctos al parámetro");
+            throw new NullPointerException();
+        }
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @return the puerto
+     */
+    public String getPuerto() {
+        return puerto;
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    public boolean esIniciable()
+    {
+        if(!((this.id == null || this.id.equals("")) &&
+            (this.address == null || this.address.equals("")) &&
+            (this.puerto == null || this.puerto.equals(""))))
+            return true;
+        else return false;
+    }
+
+    /**
+     * 
+     * @param t
+     */
+    public void actualizaVector(Tupla t)
+    {
+        this.vt[t.proceso] = t.mensaje;
+    }
+
+    public void actualizaVector(int proceso, int mensaje)
+    {
+        this.vt[proceso] = mensaje;
     }
 }
